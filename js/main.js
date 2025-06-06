ef660af80dbdf526e26ea0cbfb9d232401eb2da6
HalaKH03 <160409632+HalaKH03@users.noreply.github.com>
2025-06-06 23:42:49 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,20 @@
+//swiper
+  var swiper = new Swiper(".mySwiper",
+{
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: 
+    {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: 
+    {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    
+});
+
+
+
